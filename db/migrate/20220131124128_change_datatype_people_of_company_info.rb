fb13a274e9fcdc7dfb9a4d22546d835669a378c6
EC2 Default User <ec2-user@ip-172-31-6-27.ec2.internal>
2022-02-04 18:36:18 +0000

    first

--- db/migrate/20220131124128_change_datatype_people_of_company_info.rb
+++ db/migrate/20220131124128_change_datatype_people_of_company_info.rb
@@ -1,12 +1,12 @@
 class ChangeDatatypePeopleOfCompanyInfo < ActiveRecord::Migration[5.2]
   def up
-    change_column :company_infos, :people, :integer
-    change_column :company_infos, :capital, :integer
+    change_column :company_infos, :people, :integer,using: "people::integer"
+    change_column :company_infos, :capital, :integer,using: "capital::integer"
   end
 
   def down
-    change_column :company_infos, :people, :string
-    change_column :company_infos, :capital, :string
+    change_column :company_infos, :people, :string,using: "people::string"
+    change_column :company_infos, :capital, :string,using: "capital::string"
   end
 
 end
